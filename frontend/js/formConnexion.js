6230d47b07082d430610e05cf41351a61c5ac5b9
Codeur16 <charlesnjiosseu@gmail.com>
2023-02-01 09:01:03 +0100

    page Accueil + structure des autres pages

--- frontend/js/formConnexion.js
+++ frontend/js/formConnexion.js
@@ -1,33 +1,50 @@
-var Username;
-var Password;
-const form = document.querySelector("#form");
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
+// var Username;
+// var Password;
+// const form = document.querySelector("#form");
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault();
 
-  Username = form.elements.username.value;
-  Password = form.elements.password.value;
+//   Username = form.elements.username.value;
+//   Password = form.elements.password.value;
 
-  console.log("Email :", Username);
-  console.log("Password :", Password);
-
- 
- 
+//   console.log("Email :", Username);
+//   console.log("Password :", Password);
 
  
+ 
+
+ 
+// });
+
+
+
+
+// function redirect() {
+//   if(Username !='' && Password!=''){
+//     window.location.href='../auth/accueil.html';;
+//   }
+//   else{
+//     alert("Veillew remplir tous les champs");
+//   }
+//   // if(Username !='' && Password!=''){
+//   //   window.location.href='SCHOLAR\frontend\html\auth\accueil.html'
+//   // }
+// }
+
+
+
+
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Empêche la soumission automatique du formulaire
+  const formData = new FormData(event.target); // Récupère les données du formulaire
+  console.log(formData);
+  fetch("/api/form", {
+    method: "POST",
+    body: formData
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
 });
-
-
-
-
-function redirect() {
-  if(Username !='' && Password!=''){
-    window.location.href='../auth/accueil.html';;
-  }
-  else{
-    alert("Veillew remplir tous les champs");
-  }
-  // if(Username !='' && Password!=''){
-  //   window.location.href='SCHOLAR\frontend\html\auth\accueil.html'
-  // }
-}
 
--- frontend/js/inscription.js
+++ frontend/js/inscription.js
@@ -12,4 +12,18 @@ form.addEventListener("submit", function(event) {
   console.log("role:", role);
   console.log("phone :", phone);
   alert("USERNAME="+Username+"\npassword="+Password+"\nrole="+role+"\nphone="+phone) 
+}); 
+
+
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Empêche la soumission automatique du formulaire
+  const formData = new FormData(event.target); // Récupère les données du formulaire
+  fetch("/api/form", {
+    method: "POST",
+    body: formData
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
 });
