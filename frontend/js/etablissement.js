e136e88f0d2da6b1dbca5ff05c8263b54e23fce4
Codeur16 <charlesnjiosseu@gmail.com>
2023-02-01 20:17:23 +0100

    etablissements

---
+++ frontend/js/etablissement.js
@@ -0,0 +1,12 @@
+// const select = document.getElementById("school");
+
+// fetch("#")
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEach(item => {
+//       const option = document.createElement("option");
+//       option.value = item.value;
+//       option.textContent = item.label;
+//       select.appendChild(option);
+//     });
+//   });
